ddbf05a3618e4f02ede5f679c02f9ccdb7a85855
Katariina <rikkonenkatariina@gmail.com>
2023-10-02 14:17:31 +0300

    add project files for header,footer,recipes and roulette

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouletteComponent } from './roulette/roulette.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './roulette/recipes/recipes.component';
+import { ShoppingListComponent } from './roulette/shopping-list/shopping-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouletteComponent,
+    FooterComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
